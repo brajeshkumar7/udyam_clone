ffad1f97f4f5b9fb122156e8e0ac6ed3e1f97fb5
Brajesh Kumar <kumarbrajesh7927@gmail.com>
2025-08-15 02:03:37 +0530

    Delete backend directory

--- backend/routes/submit.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { submitForm } from "../controllers/submit.controller.js";
-const router = express.Router();
-
-
-router.post("/", submitForm);
-
-export default router;
--- backend/routes/validate.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { validateAdhar_Pan } from "../controllers/validate.controller.js";
-
-const router = express.Router();
-
-router.post("/", validateAdhar_Pan);
-
-export default router;
